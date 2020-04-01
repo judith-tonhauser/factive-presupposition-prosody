675f884b8ef818996d31a486dc4f78c1c2f4d74a
judith-tonhauser <tonhauser.1@osu.edu>
2020-04-01 12:36:05 +0200

    updated exp2

--- exp2/experiment/experiment-getResults.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-pushd /Applications/aws-mturk-clt-1.3.1/bin
-./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/judith/Documents/current-research-topics/NSF-NAI/prop-att-experiments/5-prosody-factives/3-perception-1a/run-AMT-experiment/experiment.success -outputfile /Users/judith/Documents/current-research-topics/NSF-NAI/prop-att-experiments/5-prosody-factives/3-perception-1a/run-AMT-experiment/experiment.results
-popd
